ac7b5e83c55d4798941b2c4e35e0eb498169005c
artemnchuk <artemnchuk@gmail.com>
2024-05-07 22:31:21 +0200

    html

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,34 @@
 <script setup>
-import MyHello from './components/MyHello.vue';
 </script>
 
 <template>
-<h1>Vue project</h1>
+<div
+class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14"
+>
+<header class="flex justify-between border-b">
+  <div>
+    <img src="/logo.png" alt="Logo">
+      <h2>Vue Sneakers</h2>
+      <p>Shop with the best sneakers</p>
+    </img>
+  </div>
+  <ul>
+    <li><img src="/cart.svg" alt="Cart">
+      <b>1234 usd.</b>
+    </li>
+  </ul>
+  <ul>
+    <li><img src="/heart.svg" alt="Heart">
+      <b>Favorites</b>
+    </li>
+  </ul>
+  <ul>
+    <li><img src="/profile.svg" alt="Profile">
+      <b>Profile</b>
+    </li>
+  </ul>
+</header>
+  <h1>Vue Project</h1>
+</div>
 </template>
 
