416a342eb30a49e7ccbdbf3c895fd57d815b4ab8
artemnchuk <artemnchuk@gmail.com>
2024-05-08 21:52:47 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,25 +5,27 @@
 <div
 class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14"
 >
-<header class="flex justify-between border-b">
+<header class="flex justify-between border-b border-slate-300">
   <div>
-    <img src="/logo.png" alt="Logo">
+    <img src="/logo.png" alt="Logo"/>
       <h2>Vue Sneakers</h2>
       <p>Shop with the best sneakers</p>
-    </img>
   </div>
   <ul>
-    <li><img src="/cart.svg" alt="Cart">
+    <li>
+      <img src="/cart.svg" alt="Cart"/>
       <b>1234 usd.</b>
     </li>
   </ul>
   <ul>
-    <li><img src="/heart.svg" alt="Heart">
+    <li>
+      <img src="/heart.svg" alt="Heart"/>
       <b>Favorites</b>
     </li>
   </ul>
   <ul>
-    <li><img src="/profile.svg" alt="Profile">
+    <li>
+      <img src="/profile.svg" alt="Profile"/>
       <b>Profile</b>
     </li>
   </ul>
