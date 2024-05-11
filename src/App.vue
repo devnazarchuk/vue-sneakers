4aec7573034594fef61a47e22067ba0aaa6861f4
artemnchuk <artemnchuk@gmail.com>
2024-05-11 22:13:20 +0200

    litle bit layout

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,18 @@
 <script setup>
 import Header from './components/Header.vue'
+import Card from './components/Card.vue'
 </script>
 
 <template>
-  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+  <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
     <Header />
-
-    <div class="">
-      <div class="relative m-10 border border-slate-100 rounded-3xl cursor-pointer hover:translate-y-[-2px] hover:shadow-xl">
-        <img src="/like-2.svg" alt="Like" class="absolute top-0 left-0" />
-        <img src="/sneakers/sneakers-1.jpg" alt="Sneaker" />
-        <p>Men's Sneakers Nike Blazer Suede</p>
-
-        <div class="flex justify-between">
-          <div class="flex flex-col">
-            <span class="text-slate-400">Prise:</span>
-            <b>100$</b>
-          </div>
-          <img src="/plus.svg" alt="Plus"/>
-        </div>
-      </div>
+    
+    <div>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
     </div>
   </div>
 </template>
---
+++ src/components/Card.vue
@@ -0,0 +1,15 @@
+<template>
+      <div class="relative m-10 border border-slate-100 rounded-3xl cursor-pointer hover:translate-y-[-2px] hover:shadow-xl">
+        <img src="/like-2.svg" alt="Like" class="absolute top-8 left-8" />
+        <img src="/sneakers/sneakers-1.jpg" alt="Sneaker" />
+        <p class="mt-2">Men's Sneakers Nike Blazer Suede</p>
+
+        <div class="flex justify-between mt-4">
+          <div class="flex flex-col">
+            <span class="text-slate-400">Prise:</span>
+            <b>100$</b>
+          </div>
+          <img src="/plus.svg" alt="Plus"/>
+        </div>
+      </div>
+</template>
