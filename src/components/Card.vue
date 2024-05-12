a9526503e21798178aefda979c6b8345bd5818cd
artemnchuk <artemnchuk@gmail.com>
2024-05-12 06:41:16 +0200

    cards s complate (close)

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,10 +1,16 @@
+<script setup>
+defineProps({
+  imageUrl : String,
+  title : String
+})
+</script>
 <template>
-      <div class="relative bg-yellow m-10 border border-slate-100 rounded-3xl cursor-pointer hover:translate-y-[-2px] hover:shadow-xl">
-        <img src="/like-2.svg" alt="Like" class="absolute top-8 left-8" />
-        <img src="/sneakers/sneakers-1.jpg" alt="Sneaker" />
-        <p class="mt-2">Men's Sneakers Nike Blazer Suede</p>
+      <div class="relative m-10 bg-yellow border border-slate-100 rounded-3xl cursor-pointer hover:translate-y-[-2px] hover:shadow-xl">
+        <img src="/like-2.svg" alt="Like" class="absolute top-1 left-1" />
+        <img :src="imageUrl" alt="Sneaker" />
+        <p class="m-2">{{ title }}</p>
 
-        <div class="flex justify-between mt-4">
+        <div class="flex justify-between m-2">
           <div class="flex flex-col">
             <span class="text-slate-400">Prise:</span>
             <b>100$</b>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -3,10 +3,10 @@ import Card from './Card.vue'
 </script>
 <template>
   <div class="grid grid-cols-4 gap-4">
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
+    <Card title="Air Force" imageUrl="/sneakers/sneakers-1.jpg" alt="Sneaker"/>
+    <Card title="Air Jordan" imageUrl="/sneakers/sneakers-2.jpg" alt="Sneaker"/>
+    <Card title="Converse" imageUrl="/sneakers/sneakers-3.jpg" alt="Sneaker"/>
+    <Card title= "Air Max" imageUrl="/sneakers/sneakers-4.jpg" alt="Sneaker"/>
+    <Card title= "Blazer" imageUrl="/sneakers/sneakers-5.jpg" alt="Sneaker"/>
   </div>
 </template>
