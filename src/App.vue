7d2d005b2c9370d7732b8c49eccd86ecdde773e2
artemnchuk <artemnchuk@gmail.com>
2024-05-13 22:37:56 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -11,19 +11,21 @@ import Drawer from './components/Drawer.vue'
     <div class="p-10">
       <div class="flex justify-between items-center">
         <h2 class="text-3xl font-bold mb-8">Sneakers for every occassion</h2>
-        <select class="py-2 px-3 border rounded-md outline-none">
-          <option>Filter by name</option>
-          <option>Filter by price(exspensive)</option>
-          <option>Filter by price(cheap)</option>
-          <option>Filter by brand</option>
-        </select>
-        <div class="relative">
-          <img class="absolute left-4 top-3" src="/search.svg" />
-          <input
-            type="text"
-            class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
-            placeholder="Search"
-          />
+        <div class="flex gap-4">
+          <select class="py-2 px-3 border rounded-md outline-none">
+            <option>Filter by name</option>
+            <option>Filter by price(exspensive)</option>
+            <option>Filter by price(cheap)</option>
+            <option>Filter by brand</option>
+          </select>
+          <div class="relative">
+            <img class="absolute left-4 top-3" src="/search.svg" />
+            <input
+              type="text"
+              class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
+              placeholder="Search"
+            />
+          </div>
         </div>
       </div>
       <CardList />
