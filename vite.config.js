347caf4d9f307ccbd98755108c936bdbdeea865d
artemnchuk <artemnchuk@gmail.com>
2024-05-19 17:10:02 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-sneakers/',
+  // base: '/vue-sneakers/',
   plugins: [
     vue(),
     vueJsx(),
