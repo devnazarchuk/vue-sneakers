b889fa3582718399bdf3c56275cef492fd6b831a
artemnchuk <artemnchuk@gmail.com>
2024-05-26 22:31:18 +0200

    + shop icon + adaptive

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,32 +7,38 @@ const emit = defineEmits(['openDrawer'])
 </script>
 
 <template>
-  <header class="Mesh flex justify-between border-b border-slate-100 px-10 py-4 flex-wrap">
-    <router-link to="/">
-      <div class="flex items-center gap-4 h-full">
-        <img src="/public/favicon_logo/android-chrome-512x512.png" alt="Logo" class="w-16" />
+  <header class="Mesh flex justify-between border-b border-slate-100 px-10 py-4 flex-wrap gap-x-[5vw]">
+    <router-link to="/" class="mx-auto text-center">
+      <div class="flex items-center gap-4 h-full flex-wrap justify-center">
+        <img src="/public/favicon_logo/android-chrome-512x512.png" alt="Logo" class="imgLogo" />
         <div>
           <h2 class="text-xl font-bold uppercase">✨Vue Sneakers✨</h2>
           <p class="text-slate-400">Shop with the best sneakers</p>
         </div>
       </div></router-link
     >
-    <ul class="flex items-center gap-10 mt-3">
+    <ul class="flex items-center gap-[5vw] mt-3 mx-auto flex-wrap">
+      <router-link to="/">
+      <li class="flex items-center gap-1 cursor-pointer text-gray-500 hover:text-black">
+        <img src="/shop.svg" alt="Shop" />
+        <span>Shop</span>
+      </li>
+      </router-link>
       <li
         @click="emit('openDrawer')"
-        class="flex items-center gap-3 cursor-pointer text-gray-500 hover:text-black"
+        class="flex items-center gap-1 cursor-pointer text-gray-500 hover:text-black"
       >
         <img src="/cart.svg" alt="Cart" />
         <span>{{ totalPrice }}$</span>
       </li>
       <router-link to="/favorites">
-        <li class="flex items-center gap-3 cursor-pointer text-gray-500 hover:text-black">
+        <li class="flex items-center gap-1 cursor-pointer text-gray-500 hover:text-black">
           <img src="/heart.svg" alt="Heart" />
           <span>Favorites</span>
         </li></router-link
       >
       <router-link to="/orders">
-      <li class="flex items-center gap-3 cursor-pointer text-gray-500 hover:text-black">
+      <li class="flex items-center gap-1 cursor-pointer text-gray-500 hover:text-black">
         <img src="/order.svg" alt="Orders" />
         <span>Orders</span>
       </li>
@@ -41,5 +47,12 @@ const emit = defineEmits(['openDrawer'])
   </header>
 </template>
 <style>
-
+  .imgLogo{
+    width: 4rem;
+  }
+@media screen and (max-width: 425px) {
+  .imgLogo{
+    width: 10rem;
+  }
+}
 </style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -107,20 +107,20 @@ watch(filters, fetchItems)
 </script>
 
 <template>
-  <div class="Mesh flex justify-between items-center flex-wrap items-baseline">
-    <h2 class="text-3xl font-bold mb-8 text-center">Sneakers for every occassion</h2>
+  <div class="flex justify-between items-center flex-wrap items-baseline">
+    <h2 class="text-3xl font-bold mb-8 text-center mx-auto">Sneakers for every occassion</h2>
 
-    <div class="flex gap-4 flex-wrap Mesh">
+    <div class="flex gap-4 flex-wrap mx-auto">
       <select
         @change="onChangeSelect"
-        class="py-2 px-3 border rounded-xl outline-none selectCustom"
+        class="py-2 px-3 border rounded-xl outline-none selectCustom mx-auto"
       >
         <option value="name">Filter by name</option>
         <option value="price">Filter by price(cheap)</option>
         <option value="-price">Filter by price(exspensive)</option>
       </select>
 
-      <div class="relative selectCustom">
+      <div class="relative selectCustom mx-auto">
         <img class="absolute left-4 top-3" src="/search.svg" />
         <input
           @input="onChangeSearchInput"
@@ -143,12 +143,5 @@ watch(filters, fetchItems)
   box-shadow: 1px 1px #ccc;
   border-radius: 0.75rem;
 }
-.Mesh {
-  justify-content: space-between;
-}
-@media screen and (max-width: 1024px) {
-  .Mesh {
-    justify-content: space-evenly;
-  }
-}
+
 </style>
